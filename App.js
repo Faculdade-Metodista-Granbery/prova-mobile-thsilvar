19a00a20bae194a7949347c046dfc49ada1c9c97
Thiago Silva <thsilvar@gmail.com>
2021-05-27 21:33:56 -0300

    Update App.js

--- App.js
+++ App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, FlatList, StatusBar } from 'react-native';
 import CardQuote from './components/card/card.component';
 
-const notList = [
-  { id: 1, task: 'Suco de gratidão + clorofila', background: 'https://image.freepik.com/free-vector/flat-night-sky-background_23-2148032671.jpg'},
-  { id: 2, task: 'Aplaudir o por do sol', background: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTevLH9vqcGBf4kYYXN1sEafET9xBaEjxUOMg&usqp=CAU'},
-  { id: 3, task: '5 séries de namastê', background: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa6OUSY2144YwI6mFLlxCKdyvkmKn6yLEoLA&usqp=CAU'},
+const list = [
+  { id: 1, task: 'Suco de gratidão + clorofila', background: 'https://image.freepik.com/free-vector/flat-night-sky-background_23-2148032671.jpg' },
+  { id: 2, task: 'Aplaudir o por do sol', background: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTevLH9vqcGBf4kYYXN1sEafET9xBaEjxUOMg&usqp=CAU' },
+  { id: 3, task: '5 séries de namastê', background: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa6OUSY2144YwI6mFLlxCKdyvkmKn6yLEoLA&usqp=CAU' },
 ]
 
 export default function App() {
@@ -14,11 +14,18 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <StatusBar
         animated={true}
-        backgroundColor="#fff
-       />
-          <CardQuote />
-
-
+        backgroundColor="#c64242"
+      />
+      <FlatList
+        data={list}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) =>
+          <CardQuote text={item.text}
+            author={item.author} profileImg=
+            {item.profileImg} />
+        }
+      >
+      </FlatList>
     </SafeAreaView>
   );
 }
